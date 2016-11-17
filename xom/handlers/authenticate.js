b344e6b1e9ebb2dbaa8a852d7342bb784e5a6bbf
Jared Hanson <jaredhanson@gmail.com>
2016-11-16 16:37:53 -0800

    Handle authentication failures in-state.

--- xom/handlers/authenticate.js
+++ xom/handlers/authenticate.js
@@ -1,39 +1,54 @@
 exports = module.exports = function(authenticator, ceremony, stateStore) {
-  var clone = require('clone');
-  
+  var errors = require('http-errors');
   
   return [
     ceremony.loadState('login'),
     authenticator.authenticate('local', { failWithError: true }),
-    ceremony.resume('login'),
-    function(err, req, res, next) {
-      console.log(err);
-      if (err.status == 401) { // Unauthorized
-        var state = req.state ? clone(req.state) : { name: 'login' };
-        state.failureCount = state.failureCount ? state.failureCount + 1 : 1;
-        
-        function proceed(err, h) {
-          if (err) { return next(err); }
-          
-          req.state = state;
-          req.state.handle = h;
-          next();
-        }
-        
-        if (req.state) {
-          stateStore.update(req, req.state.handle, state, proceed);
-        } else {
-          if (req.body.state) {
-            state.prev = req.body.state;
-          }
-          
-          stateStore.save(req, state, proceed);
-        }
-        return;
+    function unauthorizedErrorHandler(err, req, res, next) {
+      if (err.status !== 401) { return next(err); }
+      
+      console.log('HANDLE THE AUTH ERROR...');
+      console.log(req.state);
+      console.log(req.session.state);
+      console.log('---');
+      
+      var state = req.state || { name: 'login' };
+      state.failureCount = state.failureCount ? state.failureCount + 1 : 1;
+      
+      console.log('CHECK IT');
+      console.log(state);
+      
+      // This allows the
+    // ceremony to re-prompt the user for credentials a configurable number of
+    // times, redirecting back to the client if and when the limit is exceeded.
+      if (state.maxAttempts && state.failureCount >= state.maxAttempts) {
+        return next(new errors.Unauthorized('Too many login attempts'));
       }
-      next(err);
+      
+      
+      function proceed(ierr, h) {
+        if (ierr) { return next(ierr); }
+        
+        //req.state = state;
+        res.locals.state = h;
+        res.render('login');
+      }
+      
+      if (req.state) {
+        console.log('UPDATE STATE');
+        
+        stateStore.update(req, req.state.handle, state, proceed);
+      } else {
+        console.log('SAVE NEW STATE');
+        
+        if (req.body.state) {
+          state.prev = req.body.state;
+        }
+        stateStore.save(req, state, proceed);
+      }
     },
-    ceremony.resumeError('login')
+    ceremony.resume('login'),
+    ceremony.resumeError('login'),
   ];
   
 };
