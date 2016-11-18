95ac3d0677d10b1fc599b3819a74a929b80e2195
Jared Hanson <jaredhanson@gmail.com>
2016-11-17 17:44:07 -0800

    Add OAuth 2.0 return handler.

---
+++ xom/handlers/oauth2/return.js
@@ -0,0 +1,19 @@
+exports = module.exports = function(ceremony, authenticator) {
+  var flowstate = require('flowstate');
+
+  return [
+    ceremony.loadState('login/oauth2'),
+    // NOTE: Running without sessions causes a new login prompt and new oath transaction.  why isn't it the same?
+    //authenticator.authenticate('google.com', { session: false, failWithError: true }),
+    // TODO: Disable session and localize account
+    authenticator.authenticate('google.com', { session: true, failWithError: true }),
+    ceremony.complete('login/oauth2'),
+    ceremony.completeWithError('login/oauth2')
+  ];
+  
+};
+
+exports['@require'] = [
+  'http://i.bixbyjs.org/http/ua/flows/Dispatcher',
+  'http://i.bixbyjs.org/http/Authenticator'
+];
