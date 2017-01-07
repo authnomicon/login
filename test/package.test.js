f361eff3d84f7cc867fb99dab9101706fff0304e
Jared Hanson <jaredhanson@gmail.com>
2017-01-07 18:00:53 +1300

    Remove invalid test.

--- test/package.test.js
+++ test/package.test.js
@@ -4,10 +4,6 @@ var pkg = require('..');
 var expect = require('chai').expect;
 
 
-describe('nodex-aaa-login', function() {
-  
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
-  });
+describe('nodex-login', function() {
   
 });
