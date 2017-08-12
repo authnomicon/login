3d035a061e415cdddce05ef194068536a3d4f0db
Jared Hanson <jaredhanson@gmail.com>
2017-08-11 17:09:14 -0700

    Add session manager.

--- app/session/manager.js
+++ app/session/manager.js
@@ -1,8 +1,21 @@
 exports = module.exports = function(serializeUser, deserializeUser) {
+  
+  /*
+  var SessionManager = require('../../lib/sessionmanager');
+  
+  var smgr = new SessionManager();
+  smgr.serializeUser(serializeUser);
+  smgr.deserializeUser(deserializeUser);
+  
+  return smgr;
+  */
+  
+  
   return {
     serializeUser: serializeUser,
     deserializeUser: deserializeUser
   }
+  
 };
 
 exports['@implements'] = 'http://i.bixbyjs.org/http/auth/SessionManager';
