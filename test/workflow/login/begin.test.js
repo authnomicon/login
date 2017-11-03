f7d8805255bb5ef9dad24e367a4e4f1fce634d3c
Jared Hanson <jaredhanson@gmail.com>
2017-11-02 17:45:58 -0700

    Add test cases.

--- test/workflow/login/begin.test.js
+++ test/workflow/login/begin.test.js
@@ -1,5 +1,6 @@
 /* global describe, it, expect */
 
+var chai = require('chai');
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var factory = require('../../../app/workflow/login/begin');
@@ -10,5 +11,31 @@ describe('workflow/login/begin', function() {
   it('should export factory function', function() {
     expect(factory).to.be.a('function');
   });
+  
+  describe('handler', function() {
+    
+    describe('default behavior', function() {
+      var response;
+      
+      before(function(done) {
+        var handler = factory();
+        
+        chai.express.handler(handler)
+          .end(function(res) {
+            console.log('END!')
+            response = res;
+            done();
+          })
+          .dispatch();
+      });
+      
+      it('should redirect', function() {
+        expect(response.statusCode).to.equal(302);
+        expect(response.getHeader('Location')).to.equal('/login');
+      });
+      
+    }); // default behavior
+    
+  }); // handler
   
 });
