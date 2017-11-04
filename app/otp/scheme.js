722d322ecdcdefa2e9438a6409d8274e1646be31
Jared Hanson <jaredhanson@gmail.com>
2017-11-03 17:47:24 -0700

    Update OTP scheme.

--- app/otp/scheme.js
+++ app/otp/scheme.js
@@ -1,11 +1,12 @@
-exports = module.exports = function(verify) {
+exports = module.exports = function(otp, verify) {
   var Strategy = require('passport-otp');
   
-  return new Strategy({ passReqToCallback: true }, verify);
+  return new Strategy({ passReqToCallback: true }, otp, verify);
 };
 
 exports['@implements'] = 'http://i.bixbyjs.org/http/auth/Scheme';
 exports['@scheme'] = 'www/otp';
 exports['@require'] = [
+  'http://schemas.authnomicon.org/js/security/authentication/otp',
   './scheme/verify'
 ];
