183ce967f4e2a2d288dd3126378282a97aed8e55
Jared Hanson <jaredhanson@gmail.com>
2017-11-08 11:40:19 +0100

    Separate generation from rendering.

--- app/otp/handlers/enroll/new.js
+++ app/otp/handlers/enroll/new.js
@@ -1,28 +1,31 @@
 exports = module.exports = function(OTP, csrfProtection) {
   var path = require('path');
   
+  function generate(req, res, next) {
+    var type = req.query.algorithm || 'totp';
+    
+    OTP.generate(type, function(err, authnr) {
+      if (err) { return next(err); }
+      
+      var qr = 'https://chart.googleapis.com/chart?chs=166x166&chld=L|0&cht=qr&chl=' + encodeURIComponent(authnr.barcodeURL);
+      
+      res.locals.barcodeURL = qr;
+      next();
+    });
+  }
+  
   function prompt(req, res, next) {
     res.locals.state = req.query.state;
     res.locals.csrfToken = req.csrfToken();
     
-    
-    OTP.generate('totp', function(err, out) {
-      if (err) { return cb(err); }
-      
-     
-      var qr = 'https://chart.googleapis.com/chart?chs=166x166&chld=L|0&cht=qr&chl=' + encodeURIComponent(out.barcodeURL);
-      
-      res.locals.csrfToken = req.csrfToken();
-      res.locals.barcodeURL = qr;
-      
-      var view = path.join(__dirname, '../../../../views/otp/enroll.ejs');
-      res.render(view);
-    });
+    var view = path.join(__dirname, '../../../../views/otp/enroll.ejs');
+    res.render(view);
   }
   
   
   return [
     csrfProtection(),
+    generate,
     prompt
   ];
 };
