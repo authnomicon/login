832d557d59ebc479c93fd1eef1595fc119a4d0a5
Jared Hanson <jaredhanson@gmail.com>
2018-02-25 08:58:34 -0800

    Temporarily remove data services (need to port to realm and directory).

--- app/state/scheme/verify.js
+++ app/state/scheme/verify.js
@@ -3,13 +3,18 @@ exports = module.exports = function(Users) {
   return function(uid, state, cb){
     uid = '5001';
     
+    console.log('TODO: authnomicon-login, state/scheme/verify - port to realms');
+    /*
     if (!uid) { return cb(null, false); }
     Users.get(uid, function(err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
       return cb(null, user);
     });
+    */
   };
 };
 
-exports['@require'] = [ 'http://schemas.authnomicon.org/js/ds/Users' ];
+exports['@require'] = [
+  //'http://schemas.authnomicon.org/js/ds/Users'
+];
