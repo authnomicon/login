80c7e61280f390116fba844f9c9718a7a4fb424a
Jared Hanson <jaredhanson@gmail.com>
2018-04-05 07:39:19 -0700

    Adding login service.

---
+++ app/http/login/handlers/prompt.js
@@ -0,0 +1,36 @@
+exports = module.exports = function(ceremony, csrfProtection, authenticate, errorLogging) {
+  var path = require('path')
+    , ejs = require('ejs')
+  
+  
+  function render(req, res, next) {
+    res.render('loginx', function(err, str) {
+      if (err && err.view) {
+        var view = path.resolve(__dirname, '../../password/views/login.ejs');
+        ejs.renderFile(view, res.locals, function(err, str) {
+          if (err) { return next(err); }
+          res.send(str);
+        });
+        return;
+      } else if (err) {
+        return next(err);
+      }
+      res.send(str);
+    });
+  }
+  
+  
+  return ceremony('login',
+    //authenticate([ 'session', 'anonymous' ]),
+    csrfProtection(),
+    render,
+    errorLogging()
+  );
+};
+
+exports['@require'] = [
+  'http://i.bixbyjs.org/http/middleware/ceremony',
+  'http://i.bixbyjs.org/http/middleware/csrfProtection',
+  'http://i.bixbyjs.org/http/middleware/authenticate',
+  'http://i.bixbyjs.org/http/middleware/errorLogging'
+];
---
+++ app/http/login/service.js
@@ -0,0 +1,15 @@
+exports = module.exports = function(promptHandler) {
+  var express = require('express');
+  var router = new express.Router();
+  
+  router.get('/', promptHandler);
+  
+  return router;
+};
+
+exports['@implements'] = [
+  'http://i.bixbyjs.org/http/Service',
+  'http://schemas.authnomicon.org/js/http/login/Service'
+];
+exports['@path'] = '/login';
+exports['@require'] = [ './handlers/prompt' ];
