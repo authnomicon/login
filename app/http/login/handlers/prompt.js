d8a0191552c80d95f74f3c7c5dd52597ad356605
Jared Hanson <jaredhanson@gmail.com>
2018-04-06 18:08:55 -0700

    Consolidating render.

--- app/http/login/handlers/prompt.js
+++ app/http/login/handlers/prompt.js
@@ -1,31 +1,9 @@
-exports = module.exports = function(ceremony, csrfProtection, authenticate, errorLogging) {
-  var path = require('path')
-    , ejs = require('ejs')
-  
-  
-  function render(req, res, next) {
-    res.locals.csrfToken = req.csrfToken();
-    
-    res.render('loginx', function(err, str) {
-      if (err && err.view) {
-        var view = path.resolve(__dirname, '../../password/views/login.ejs');
-        ejs.renderFile(view, res.locals, function(err, str) {
-          if (err) { return next(err); }
-          res.send(str);
-        });
-        return;
-      } else if (err) {
-        return next(err);
-      }
-      res.send(str);
-    });
-  }
-  
+exports = module.exports = function(render, ceremony, csrfProtection, authenticate, errorLogging) {
   
   return [
     ceremony('login',
-      //authenticate([ 'session', 'anonymous' ]),
       csrfProtection(),
+      //authenticate([ 'session', 'anonymous' ]),
       render
     ),
     errorLogging()
@@ -33,6 +11,7 @@ exports = module.exports = function(ceremony, csrfProtection, authenticate, erro
 };
 
 exports['@require'] = [
+  '../include/render', 
   'http://i.bixbyjs.org/http/middleware/ceremony',
   'http://i.bixbyjs.org/http/middleware/csrfProtection',
   'http://i.bixbyjs.org/http/middleware/authenticate',
---
+++ app/http/login/include/render.js
@@ -0,0 +1,25 @@
+exports = module.exports = function() {
+  var path = require('path')
+    , ejs = require('ejs')
+  
+  
+  return function render(req, res, next) {
+    res.locals.csrfToken = req.csrfToken();
+    
+    res.render('loginx', function(err, str) {
+      if (err && err.view) {
+        var view = path.resolve(__dirname, '../../password/views/login.ejs');
+        ejs.renderFile(view, res.locals, function(err, str) {
+          if (err) { return next(err); }
+          res.send(str);
+        });
+        return;
+      } else if (err) {
+        return next(err);
+      }
+      res.send(str);
+    });
+  };
+}
+
+exports['@require'] = [];
