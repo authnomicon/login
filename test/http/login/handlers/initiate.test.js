2ca8bea7d29e3d3697785a948b0ef53afbaa32e1
Jared Hanson <jaredhanson@gmail.com>
2018-05-15 13:10:01 -0700

    Stub in initiate test.

---
+++ test/http/login/handlers/initiate.test.js
@@ -0,0 +1,21 @@
+/* global describe, it, expect */
+
+var chai = require('chai');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var factory = require('../../../../app/http/login/handlers/initiate');
+var flowstate = require('flowstate');
+
+
+describe('http/login/handlers/initiate', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.be.undefined;
+    expect(factory['@singleton']).to.be.undefined;
+  });
+  
+});
