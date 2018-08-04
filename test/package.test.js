2424e5b13229e468a166a7954edf3498c41d21ae
Jared Hanson <jaredhanson@gmail.com>
2018-08-04 11:45:22 -0700

    Add tests.

--- test/package.test.js
+++ test/package.test.js
@@ -5,6 +5,17 @@ var expect = require('chai').expect;
 
 describe('@authnomicon/login', function() {
   
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('org.authnomicon/login');
+      
+      expect(json.assembly.components).to.have.length(11);
+      expect(json.assembly.components).to.include('password/http/service');
+    });
+  });
+  
   it('should throw if required', function() {
     expect(function() {
       var pkg = require('..');
