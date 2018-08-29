f2d6690d716c82dd50cbf669154ece2e846b0828
Jared Hanson <jaredhanson@gmail.com>
2018-08-29 12:25:03 -0700

    Clean up state authentication.

--- app/http/state/verify.js
+++ app/http/state/verify.js
@@ -1,26 +1,8 @@
 exports = module.exports = function(realms) {
 
-  return function(uid, state, cb){
-    console.log('TODO: authnomicon-login, state/scheme/verify - port to realms');
-    //console.log(uid)
-    //console.log(state);
-    
-    //uid = '5001';
-    
-    if (!uid) { return cb(null, false); }
-    
-    var subject = { id: uid };
-    return cb(null, subject);
-    
-    
-    /*
-    if (!uid) { return cb(null, false); }
-    Users.get(uid, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      return cb(null, user);
-    });
-    */
+  return function(user, state, cb) {
+    if (!user) { return cb(null, false); }
+    return cb(null, user);
   };
 };
 
