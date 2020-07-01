836cca35fec306a2ef085afaed8879beb1e0aa1f
Jared Hanson <jaredhanson@gmail.com>
2020-07-01 10:07:57 -0700

    Remove unused session components.

--- app/http/session/serializeuser.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = function() {
-
-  return function(user, cb) {
-    // TODO: Serialize set of commonly needed properties
-    return cb(null, user.id);
-  };
-};
-
-exports['@require'] = [];
