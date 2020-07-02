1be761a5189593af66f77d041c44cfb81e7717a7
Jared Hanson <jaredhanson@gmail.com>
2020-07-02 16:35:57 -0700

    Add comments.

--- app/otp/http/service.js
+++ app/otp/http/service.js
@@ -1,7 +1,23 @@
+/**
+ * OTP authentication service.
+ *
+ * This component provides an HTTP service that authenticates an end-user using
+ * a one-time password (OTP).
+ *
+ * This service is intended to be used by sites making use of HTML forms to
+ * present the end-user with an interface for logging in to the site.  The
+ * interface is rendered by the web browser being used by the end-user.  After
+ * authentication, the login session is stepped up to indicate the additional
+ * factor of authentication.
+ *
+ * @param {Function|Function[]} promptHandler - Prompt handler.
+ * @param {Function|Function[]} verifyHandler - Verify handler.
+ * @returns {Function}
+ */
 exports = module.exports = function(promptHandler, verifyHandler) {
   var express = require('express');
-  var router = new express.Router();
   
+  var router = new express.Router();
   router.get('/', promptHandler);
   router.post('/', verifyHandler);
   
@@ -9,7 +25,7 @@ exports = module.exports = function(promptHandler, verifyHandler) {
 };
 
 exports['@implements'] = 'http://i.bixbyjs.org/http/Service';
-exports['@path'] = '/login/otp';
+exports['@path'] = '/stepup/otp';
 exports['@require'] = [
   './handlers/prompt',
   './handlers/verify'
