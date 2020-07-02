97af2038e35378ff941f6221b9352a3ed5805a0b
Jared Hanson <jaredhanson@gmail.com>
2020-07-02 15:46:38 -0700

    Clean up password verification and add tests.

--- test/password/http/scheme.test.js
+++ test/password/http/scheme.test.js
@@ -19,22 +19,227 @@ describe('password/http/scheme', function() {
     expect(factory['@singleton']).to.be.undefined;
   });
   
-  describe('creating scheme', function() {
+  describe('verifying username and password', function() {
+    var passwords = new Object();
+    passwords.verify = sinon.stub().yieldsAsync(null, true);
+    var users = new Object();
+    users.find = sinon.stub().yieldsAsync(null, { id: '248289761001', displayName: 'Jane Doe' });
+    
     var StrategySpy = sinon.spy(Strategy);
-    var verify = function(){};
     
     var factory = $require('../../../app/password/http/scheme',
       { 'passport-local': StrategySpy });
-    var strategy = factory(verify);
+    var strategy = factory(passwords, users);
     
     it('should construct strategy', function() {
       expect(StrategySpy).to.have.been.calledOnce;
-      expect(StrategySpy).to.have.been.calledWithExactly(verify);
     });
     
     it('should return strategy', function() {
       expect(strategy).to.be.an.instanceOf(Strategy);
     });
-  }); // creating scheme
+    
+    describe('verify', function() {
+      var user, info;
+      
+      before(function(done) {
+        var verify = StrategySpy.args[0][0];
+        verify('jane', 'opensesame', function(e, u, i) {
+          if (e) { return done(e); }
+          user = u;
+          info = i;
+          done();
+        });
+      });
+      
+      it('should verify credentials', function() {
+        expect(passwords.verify).to.calledOnceWith('jane', 'opensesame');
+      });
+      
+      it('should query directory', function() {
+        expect(users.find).to.have.been.calledOnceWith('jane');
+      });
+      
+      it('should yield user', function() {
+        expect(user).to.deep.equal({
+          id: '248289761001',
+          displayName: 'Jane Doe'
+        });
+      });
+      
+      it('should yield info', function() {
+        expect(info).to.deep.equal({
+          methods: [ 'password' ]
+        });
+      });
+    }); // verify
+    
+  }); // verifying username and password
+  
+  describe('verifying username and password using credential service with directory capability', function() {
+    var passwords = new Object();
+    passwords.verify = sinon.stub().yieldsAsync(null, { id: '248289761001', displayName: 'Jane Doe' });
+    var users = new Object();
+    users.find = sinon.spy();
+    
+    var StrategySpy = sinon.spy(Strategy);
+    
+    var factory = $require('../../../app/password/http/scheme',
+      { 'passport-local': StrategySpy });
+    var strategy = factory(passwords, users);
+    
+    it('should construct strategy', function() {
+      expect(StrategySpy).to.have.been.calledOnce;
+    });
+    
+    it('should return strategy', function() {
+      expect(strategy).to.be.an.instanceOf(Strategy);
+    });
+    
+    describe('verify', function() {
+      var user, info;
+      
+      before(function(done) {
+        var verify = StrategySpy.args[0][0];
+        verify('jane', 'opensesame', function(e, u, i) {
+          if (e) { return done(e); }
+          user = u;
+          info = i;
+          done();
+        });
+      });
+      
+      it('should verify credentials', function() {
+        expect(passwords.verify).to.calledOnceWith('jane', 'opensesame');
+      });
+      
+      it('should not query directory', function() {
+        expect(users.find).to.not.have.been.called;
+      });
+      
+      it('should yield user', function() {
+        expect(user).to.deep.equal({
+          id: '248289761001',
+          displayName: 'Jane Doe'
+        });
+      });
+      
+      it('should yield info', function() {
+        expect(info).to.deep.equal({
+          methods: [ 'password' ]
+        });
+      });
+    }); // verify
+    
+  }); // verifying username and password using credential service with directory capability
+  
+  describe('not verifying username and password', function() {
+    var passwords = new Object();
+    passwords.verify = sinon.stub().yieldsAsync(null, false);
+    var users = new Object();
+    users.find = sinon.stub().yieldsAsync(null, { id: '248289761001', displayName: 'Jane Doe' });
+  
+    var StrategySpy = sinon.spy(Strategy);
+  
+    var factory = $require('../../../app/password/http/scheme',
+      { 'passport-local': StrategySpy });
+    var strategy = factory(passwords, users);
+  
+    it('should construct strategy', function() {
+      expect(StrategySpy).to.have.been.calledOnce;
+    });
+  
+    it('should return strategy', function() {
+      expect(strategy).to.be.an.instanceOf(Strategy);
+    });
+  
+    describe('verify', function() {
+      var user, info;
+    
+      before(function(done) {
+        var verify = StrategySpy.args[0][0];
+        verify('jane', 'opensesame', function(e, u, i) {
+          if (e) { return done(e); }
+          user = u;
+          info = i;
+          done();
+        });
+      });
+    
+      it('should verify credentials', function() {
+        expect(passwords.verify).to.calledOnceWith('jane', 'opensesame');
+      });
+    
+      it('should not query directory', function() {
+        expect(users.find).to.not.have.been.called;
+      });
+    
+      it('should not yield user', function() {
+        expect(user).to.equal(false);
+      });
+    
+      it('should not yield info', function() {
+        expect(info).to.be.undefined;
+      });
+    }); // verify
+  
+  }); // not verifying username and password
+  
+  describe('encountering error while verifying credentials', function() {
+    var passwords = new Object();
+    passwords.verify = sinon.stub().yieldsAsync(new Error('something went wrong'));
+    var users = new Object();
+    users.find = sinon.stub().yieldsAsync(null, { id: '248289761001', displayName: 'Jane Doe' });
+  
+    var StrategySpy = sinon.spy(Strategy);
+  
+    var factory = $require('../../../app/password/http/scheme',
+      { 'passport-local': StrategySpy });
+    var strategy = factory(passwords, users);
+  
+    it('should construct strategy', function() {
+      expect(StrategySpy).to.have.been.calledOnce;
+    });
+  
+    it('should return strategy', function() {
+      expect(strategy).to.be.an.instanceOf(Strategy);
+    });
+  
+    describe('verify', function() {
+      var error, user, info;
+    
+      before(function(done) {
+        var verify = StrategySpy.args[0][0];
+        verify('jane', 'opensesame', function(e, u, i) {
+          error = e;
+          user = u;
+          info = i;
+          done();
+        });
+      });
+    
+      it('should verify credentials', function() {
+        expect(passwords.verify).to.calledOnceWith('jane', 'opensesame');
+      });
+    
+      it('should not query directory', function() {
+        expect(users.find).to.not.have.been.called;
+      });
+      
+      it('should yield error', function() {
+        expect(error).to.be.an.instanceOf(Error);
+        expect(error.message).to.equal('something went wrong');
+      });
+    
+      it('should not yield user', function() {
+        expect(user).to.be.undefined;
+      });
+    
+      it('should not yield info', function() {
+        expect(info).to.be.undefined;
+      });
+    }); // verify
+  
+  }); // not verifying username and password
   
 });
