4aa2a72044ac522831cfdef461155f0400a63a6a
Jared Hanson <jaredhanson@gmail.com>
2020-07-03 18:13:29 -0700

    Simplify login prompt.

--- app/http/handlers/prompt.js
+++ app/http/handlers/prompt.js
@@ -3,68 +3,35 @@
  *
  * This component provides an HTTP handler that prompts for login.
  */
-exports = module.exports = function(authenticationHandler, authenticate, errorLogging, ceremony) {
-  var Request = require('../../../lib/request')
-    , Response = require('../../../lib/response');
+exports = module.exports = function(csrfProtection, ceremony) {
   
   
-  function cont(req, res, next) {
-    //req.state.foo = 'bar';
+  function prompt(req, res, next) {
+    res.locals.csrfToken = req.csrfToken();
     
-    // TODO: initialize request with supported challenge types
-    var lreq = new Request(req.user)
-      , lres = new Response();
-    
-    lreq.session = req.session;
-    
-    function ondecision(result, scope) {
-      //res.redirect('/happy')
-      next();
-    }
-    
-    function onchallenge(type, options) {
-      switch (type) {
-      case 'password':
-        res.redirect('/login/password');
-        break;
-      case 'otp':
-        res.redirect('/login/otp');
-        break;
-      case 'oob':
-        res.redirect('/login/oob');
-        break;
-      default:
-        return next(new Error('Unsupported login challenge: ' + type));
+    res.render('login', function(err, str) {
+      if (err && err.view) {
+        return res.redirect('/login/password');
+      } else if (err) {
+        return next(err);
       }
-    }
-    
-    function onend() {
-      lres.removeListener('decision', ondecision);
-      lres.removeListener('__challenge__', onchallenge);
-    }
-    
-    lres.once('decision', ondecision);
-    lres.once('__challenge__', onchallenge);
-    lres.once('end', onend);
-    
-    authenticationHandler(lreq, lres);
+      res.send(str);
+    });
   }
   
   
   return [
+    csrfProtection(),
     ceremony(
-      authenticate([ 'session', 'anonymous' ]),
-      cont
+      prompt
     ),
     // Should GET requests that error with a state destroy the state?  I think not
     // There needs to be an option for it (external?) that does, for eg OAuth
-    errorLogging()
+    //errorLogging()
   ];
 };
 
 exports['@require'] = [
-  'http://i.authnomicon.org/login/authenticationHandler',
-  'http://i.bixbyjs.org/http/middleware/authenticate',
-  'http://i.bixbyjs.org/http/middleware/errorLogging',
+  'http://i.bixbyjs.org/http/middleware/csrfProtection',
   'http://i.bixbyjs.org/http/middleware/ceremony'
 ];
