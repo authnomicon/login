05b4aeae1c511cc508f7095fa958ac39df53d1d5
Jared Hanson <jaredhanson@gmail.com>
2021-10-05 07:54:25 -0700

    Allow app to override identifier router.

--- app/identifier/http/router.js
+++ app/identifier/http/router.js
@@ -1,16 +1,23 @@
-exports = module.exports = function() {
+exports = module.exports = function(container) {
   
   // http://i.authnomicon.org/login/IdentifierRouter
   
-  return function(identifier, res, next) {
-    console.log('route the resposne');
-    console.log(identifier)
+  return container.create('http://i.authnomicon.org/login/IdentifierRouter')
+    .catch(function(err) {
+      console.log('!!!(())');
+      //console.log(err);
+      
+      return function(identifier, res, next) {
+        console.log('route the resposne');
+        console.log(identifier)
     
-    // TODO: query encode this
-    res.redirect('/login/password?username=' + identifier);
-  }
+        // TODO: query encode this
+        res.redirect('/login/password?username=' + identifier);
+      }
+    });
 };
 
 exports['@singleton'] = true;
 exports['@require'] = [
+  '!container'
 ];
