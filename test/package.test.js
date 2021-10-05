c3c000dc1328eb9354abdb6e99f11401c534c20e
Jared Hanson <jaredhanson@gmail.com>
2021-10-04 21:05:54 -0700

    Update package components.

--- test/package.test.js
+++ test/package.test.js
@@ -11,7 +11,7 @@ describe('@authnomicon/login', function() {
     it('should have assembly metadata', function() {
       expect(json.assembly.namespace).to.equal('org.authnomicon/login');
       
-      expect(json.assembly.components).to.have.length(8);
+      expect(json.assembly.components).to.have.length(3);
       expect(json.assembly.components).to.include('password/http/service');
       expect(json.assembly.components).to.include('password/http/scheme');
     });
