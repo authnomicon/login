2c3d1d9efcc50d8bf64f2015f61ccdf49338f514
Jared Hanson <jaredhanson@gmail.com>
2021-10-18 07:34:51 -0700

    Clean up password prompt tests.

--- test/password/http/handlers/prompt.test.js
+++ test/password/http/handlers/prompt.test.js
@@ -8,19 +8,14 @@ var factory = require('../../../../app/password/http/handlers/prompt');
 
 describe('password/http/handlers/prompt', function() {
   
-  it('should export factory function', function() {
-    expect(factory).to.be.a('function');
-  });
+  var handler;
   
-  describe('handler', function() {
-    var test;
-    
+  before(function() {
     function csrfProtection() {
       return function(req, res, next) {
         req.csrfToken = function() {
           return 'i8XNjC4b8KVok4uw5RftR38Wgp2BFwql';
         };
-      
         next();
       };
     }
@@ -34,33 +29,27 @@ describe('password/http/handlers/prompt', function() {
     var csrfProtectionSpy;
     var stateSpy;
     
-    beforeEach(function() {
-      csrfProtectionSpy = sinon.spy(csrfProtection);
-      stateSpy = sinon.spy(state);
-      
-      var handler = factory(csrfProtectionSpy, stateSpy);
-      
-      expect(csrfProtectionSpy).to.be.calledOnce;
-      expect(stateSpy).to.be.calledOnce;
-      
-      test = chai.express.use(handler);
-    });
+    csrfProtectionSpy = sinon.spy(csrfProtection);
+    stateSpy = sinon.spy(state);
     
-    describe('challenging for username and password', function() {
-      
-      it('should render', function(done) {
-        test.finish(function() {
-          expect(this).to.have.status(200);
-          expect(this).to.render('login/password').with.deep.locals({
-            csrfToken: 'i8XNjC4b8KVok4uw5RftR38Wgp2BFwql'
-          });
-          done();
-        })
-        .listen();
-      });
-      
-    }); // challenging for username and password
+    handler = factory(csrfProtectionSpy, stateSpy);
     
-  }); // handler
+    expect(csrfProtectionSpy).to.be.calledOnce;
+    expect(stateSpy).to.be.calledOnce;
+  });
+  
+  it('should prompt for username and password', function(done) {
+    
+    chai.express.use(handler)
+      .finish(function() {
+        expect(this).to.have.status(200);
+        expect(this).to.render('login/password').with.deep.locals({
+          csrfToken: 'i8XNjC4b8KVok4uw5RftR38Wgp2BFwql'
+        });
+        done();
+      })
+      .listen();
+    
+  }); // should prompt for username and password
   
 });
--- test/password/http/service.test.js
+++ test/password/http/service.test.js
@@ -1,20 +1,14 @@
 /* global describe, it, expect */
 
 var expect = require('chai').expect;
-var sinon = require('sinon');
 var factory = require('../../../app/password/http/service');
 
 
 describe('password/http/service', function() {
   
-  it('should export factory function', function() {
-    expect(factory).to.be.a('function');
-  });
-  
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://i.bixbyjs.org/http/Service');
     expect(factory['@path']).to.equal('/login/password');
-    expect(factory['@singleton']).to.be.undefined;
   });
   
   it('should create service', function() {
