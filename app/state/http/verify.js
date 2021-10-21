d5ddce7a811aa7634adad2ac02547e97cafe92d9
Jared Hanson <jaredhanson@gmail.com>
2021-10-21 12:36:04 -0700

    Remove unused state scheme.

--- app/state/http/verify.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = function(realms) {
-
-  return function(user, state, cb) {
-    if (!user) { return cb(null, false); }
-    return cb(null, user);
-  };
-};
-
-exports['@require'] = [];
