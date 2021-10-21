9c07b19c790670724efc6eaa02e5bc833e1d1119
Jared Hanson <jaredhanson@gmail.com>
2021-10-21 14:47:36 -0700

    Silence verbose logging.

--- app/identifier/http/router.js
+++ app/identifier/http/router.js
@@ -1,16 +1,9 @@
 exports = module.exports = function(container) {
   
-  // http://i.authnomicon.org/login/IdentifierRouter
-  
   return container.create('http://i.authnomicon.org/login/IdentifierRouter')
     .catch(function(err) {
-      console.log('!!!(())');
-      //console.log(err);
       
       return function(identifier, res, next) {
-        console.log('route the resposne');
-        console.log(identifier)
-    
         // TODO: query encode this
         res.redirect('/login/password?username=' + identifier);
       }
