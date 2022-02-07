9261f1d7df584315854a09d800104aca3133d160
Jared Hanson <jaredhanson@gmail.com>
2022-02-07 13:09:47 -0800

    Fix exported components.

--- test/package.test.js
+++ test/package.test.js
@@ -11,15 +11,15 @@ describe('@authnomicon/login', function() {
     it('should have assembly metadata', function() {
       expect(json.assembly.namespace).to.equal('org.authnomicon/login');
       expect(json.assembly.components).to.deep.equal([
-        'http/service',
-        'http/prompt',
-        'identifier/http/service',
-        'password/http/service',
-        'password/http/scheme',
-        'session/select/http/service',
-        'session/select/http/prompt',
+        'login/http/service',
+        'login/http/prompt',
+        'login/identifier/http/service',
+        'login/password/http/service',
+        'login/password/http/scheme',
         'logout/http/service',
         'logout/http/prompt',
+        'session/select/http/service',
+        'session/select/http/prompt',
         'signup/http/service'
       ]);
     });
