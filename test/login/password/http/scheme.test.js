1e4d49eb52edee0f36ab26dcc7d154127677590b
Jared Hanson <jaredhanson@gmail.com>
2024-01-10 11:10:32 -0800

    Clarify tests.

--- test/login/password/http/scheme.test.js
+++ test/login/password/http/scheme.test.js
@@ -98,62 +98,32 @@ describe('password/http/scheme', function() {
     });
   }); // should not verify incorrect username and password
   
-  describe('encountering error while verifying credentials', function() {
+  it('should error when encountering and error while verifying credentials', function(done) {
     var passwords = new Object();
     passwords.verify = sinon.stub().yieldsAsync(new Error('something went wrong'));
     var users = new Object();
     users.find = sinon.stub().yieldsAsync(null, { id: '248289761001', displayName: 'Jane Doe' });
   
     var StrategySpy = sinon.spy(Strategy);
-  
     var factory = $require('../../../../com/login/password/http/scheme',
       { 'passport-local': StrategySpy });
-    var strategy = factory(passwords, users);
-  
-    it('should construct strategy', function() {
-      expect(StrategySpy).to.have.been.calledOnce;
-    });
-  
-    it('should return strategy', function() {
-      expect(strategy).to.be.an.instanceOf(Strategy);
-    });
-  
-    describe('verify', function() {
-      var error, user, info;
-    
-      before(function(done) {
-        var verify = StrategySpy.args[0][0];
-        verify('jane', 'opensesame', function(e, u, i) {
-          error = e;
-          user = u;
-          info = i;
-          done();
-        });
-      });
-    
-      it('should verify credentials', function() {
-        expect(passwords.verify).to.calledOnceWith('jane', 'opensesame');
-      });
-    
-      it('should not query directory', function() {
-        expect(users.find).to.not.have.been.called;
-      });
       
-      it('should yield error', function() {
-        expect(error).to.be.an.instanceOf(Error);
-        expect(error.message).to.equal('something went wrong');
-      });
+    var scheme = factory(passwords, users);
+    expect(StrategySpy).to.have.been.calledOnce;
+    expect(scheme).to.be.an.instanceOf(Strategy);
     
-      it('should not yield user', function() {
-        expect(user).to.be.undefined;
-      });
-    
-      it('should not yield info', function() {
-        expect(info).to.be.undefined;
-      });
-    }); // verify
-  
-  }); // encountering error while verifying credentials
+    var verify = StrategySpy.args[0][0];
+    verify('jane', 'opensesame', function(err, user, info) {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('something went wrong');
+      
+      expect(passwords.verify).to.calledOnceWith('jane', 'opensesame');
+      expect(users.find).to.not.have.been.called;
+      expect(user).to.be.undefined;
+      expect(info).to.be.undefined;
+      done();
+    });
+  }); // should error when encountering and error while verifying credentials
   
   describe('encountering error while querying directory', function() {
     var passwords = new Object();
