42debd6cefa625363692c06e85bafcb49ccf8b20
Jared Hanson <jaredhanson@gmail.com>
2024-01-10 16:40:56 -0800

    Clarify tests.

--- test/login/password/http/handlers/verify.test.js
+++ test/login/password/http/handlers/verify.test.js
@@ -2,85 +2,83 @@
 
 var expect = require('chai').expect;
 var chai = require('chai');
+var $require = require('proxyquire');
 var sinon = require('sinon');
 var factory = require('../../../../../com/login/password/http/handlers/verify');
 
 
 describe('password/http/handlers/verify', function() {
   
-  var handler;
-
-  before(function() {
-    function authenticate(mechanism) {
-      return function(req, res, next) {
-        req.login = function(user, info, cb) {
-          process.nextTick(function() {
-            req.session.user = user;
-            req.session.method = info.method;
-            cb();
-          });
-        };
-      
-        req.user = { id: '248289761001', displayName: 'Jane Doe' };
-        req.authInfo = { method: 'password' };
-        next();
-      };
-    }
+  it('should create handler', function() {
+    var bodyParserSpy = sinon.spy();
+    var csurfSpy = sinon.spy();
+    var flowstateSpy = sinon.spy();
+    var factory = $require('../../../../../com/login/password/http/handlers/verify', {
+      'body-parser': { urlencoded: bodyParserSpy },
+      'csurf': csurfSpy,
+      'flowstate': flowstateSpy
+    });
     
-    /*
-    function state() {
-      return function(req, res, next) {
-        next();
-      };
-    }
-    */
+    var scheme = new Object();
+    var authenticator = new Object();
+    authenticator.authenticate = sinon.spy();
+    var store = new Object();
+    var handler = factory(scheme, authenticator, store);
     
-    var authenticateSpy = sinon.spy(authenticate);
-    //var stateSpy = sinon.spy(state);
-    
-    handler = factory(undefined, { authenticate: authenticateSpy }, undefined);
-    
-    // TODO: Put this back
-    //expect(authenticateSpy).to.be.calledOnceWith('www-form/password');
-    //expect(stateSpy).to.be.calledOnce;
+    expect(handler).to.be.an('array');
+    expect(bodyParserSpy).to.be.calledOnce;
+    expect(bodyParserSpy).to.be.calledBefore(csurfSpy);
+    expect(bodyParserSpy).to.be.calledWith({ extended: false });
+    expect(csurfSpy).to.be.calledOnce;
+    expect(csurfSpy).to.be.calledBefore(flowstateSpy);
+    expect(flowstateSpy).to.be.calledOnce;
+    expect(flowstateSpy).to.be.calledWith({ store: store });
+    expect(flowstateSpy).to.be.calledBefore(authenticator.authenticate);
+    expect(authenticator.authenticate).to.be.calledOnce;
+    expect(authenticator.authenticate).to.be.calledWith(scheme);
   });
   
-  it('verifying username and password', function(done) {
+  describe('handler', function() {
     
-    chai.express.use(handler)
-      .request(function(req, res) {
-        req.connection = {};
-        req.method = 'POST';
-        req.body = {
-          username: 'jane',
-          password: 'opensesame',
-          csrf_token: '3aev7m03-1WTaAw4lJ_GWEMkjwFBu_lwNWG8'
-        };
-        req.session = {
-          csrfSecret: 'zbVXAFVVUSXO0_ZZLBYVP9ue'
-        };
-        
-        res.resumeState = sinon.spy(function(cb) {
-          process.nextTick(cb);
-        });
-      })
-      .finish(function() {
-        expect(this.req.session).to.deep.equal({
-          csrfSecret: 'zbVXAFVVUSXO0_ZZLBYVP9ue',
-          user: {
+    var mockAuthenticator = new Object();
+    mockAuthenticator.authenticate = function(name, options) {
+      return function(req, res, next) {
+        req.user = { id: '248289761001', displayName: 'Jane Doe' };
+        next();
+      };
+    };
+    var noopStateStore = new Object();
+    
+    
+    it('should redirect as final handler', function(done) {
+      var handler = factory(undefined, mockAuthenticator, noopStateStore);
+      
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.method = 'POST';
+          req.body = {
+            username: 'jane',
+            password: 'opensesame',
+            csrf_token: '3aev7m03-1WTaAw4lJ_GWEMkjwFBu_lwNWG8'
+          };
+          req.session = {
+            csrfSecret: 'zbVXAFVVUSXO0_ZZLBYVP9ue'
+          };
+          req.connection = {};
+        })
+        .finish(function() {
+          expect(this.req.user).to.deep.equal({
             id: '248289761001',
             displayName: 'Jane Doe'
-          },
-          method: 'password'
-        });
-        
-        expect(this.statusCode).to.equal(302);
-        expect(this.getHeader('Location')).to.equal('/');
-        
-        done();
-      })
-      .listen();
-      
-  }); // authenticating username and password
+          });
+          
+          expect(this.statusCode).to.equal(302);
+          expect(this.getHeader('Location')).to.equal('/');
+          done();
+        })
+        .listen();
+    }); // should redirect as final handler
+    
+  }); // handler
   
 });
