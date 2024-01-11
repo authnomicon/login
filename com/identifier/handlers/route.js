be7ea656329aa707cf46a72f176bdc7b3a29b4e4
Jared Hanson <jaredhanson@gmail.com>
2024-01-11 12:41:14 -0800

    Add comments.

--- com/identifier/handlers/route.js
+++ com/identifier/handlers/route.js
@@ -1,9 +1,18 @@
+/**
+ * Create identifier routing handler.
+ *
+ * Returns an HTTP handler that challenges the user to authenticate with a
+ * method that varies on the user's identifier.  The identifier is submitted via
+ * an HTML form which was rendered by the `prompt` handler.
+ *
+ * @param {flowstate.Store} store - Per-request state store.
+ * @returns {express.RequestHandler[]}
+ */
 exports = module.exports = function(router, store) {
   
   function route(req, res, next) {
     router(req.body.identifier, res, next);
   }
-  
   
   
   return [
