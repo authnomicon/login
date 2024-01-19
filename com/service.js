e47f62ae8df514490693853e5e5aad337b612214
Jared Hanson <jaredhanson@gmail.com>
2024-01-19 15:00:05 -0800

    Add JSDoc.

--- com/service.js
+++ com/service.js
@@ -2,7 +2,19 @@
 var express = require('express');
 
 /**
- * Login service.
+ * Create authentication service.
+ *
+ * Returns an HTTP service that challenges a user to authenticate.  The
+ * challenge intentionally does not mandate any specific authentication method.
+ * Rather, it allows the application to present its preferred initial prompt.
+ *
+ * If the application supports multiple authentication methods, this initial
+ * prompt can present multiple methods.  This, in turn, allows the user to
+ * select their preferred authentication method.
+ *
+ * @param {express.RequestHandler} promptHandler - Handler which prompts the
+ *          user to log in.
+ * @returns {express.Router}
  */
 exports = module.exports = function(promptHandler) {
   var router = express.Router();
